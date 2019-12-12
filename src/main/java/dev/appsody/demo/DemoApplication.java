747c0ffad92ef6ff62aec2efc832c0b41674a310
Kieran Geoghegan <Kieran.Geoghegan@ie.ibm.com>
2019-12-12 12:16:08 +0000

    first commit

---
+++ src/main/java/dev/appsody/demo/DemoApplication.java
@@ -0,0 +1,10 @@
+package dev.appsody.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/*DemoApp*/
+@ApplicationPath("/demo")
+public class DemoApplication extends Application {
+
+}
---
+++ src/main/java/dev/appsody/demo/Logout.java
@@ -0,0 +1,29 @@
+package dev.appsody.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		handleRequest(request, response);
+	}
+
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		 HttpSession session = request.getSession(false);
+		 
+		 if(session!=null) {
+			 session.invalidate();
+		 }
+		 
+		 response.sendRedirect("./index.html");
+	}
+}
